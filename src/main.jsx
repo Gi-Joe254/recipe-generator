757095f5373091d07703f274ba1f93b78db7ccd9
Joe <jgacheru504@gmail.com>
2025-09-09 14:52:57 +0300

    deleted some components from unrelated folder/project

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Typing from './typing.jsx'
-import './index.css'
-import App from './App.jsx'
+import './main.css'
 import Chef from './Chef.jsx'
 
 createRoot(document.getElementById('root')).render(
